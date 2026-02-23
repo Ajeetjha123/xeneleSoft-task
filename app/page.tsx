72d1c35414d12adcbbe52e76d7b5b00a40b3ee37
AJeet Kumar Jha <kumarjhaajeet653@gmail.com>
2026-02-23 17:19:46 +0530

    added button in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-  <></>
+  <div className="flex justify-end items-center gap-10">
+          <Link href="/auth/login" className='bg-purple-700 rounded-4xl py-1 px-5 cursor-pointer text-white'>Login</Link>
+          <Link href="/auth/signup" className='bg-purple-700 rounded-4xl py-1 cursor-pointer px-5 text-white'>SignUp</Link>
+
+  </div>
   );
 }
